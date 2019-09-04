6131532ac989d072ab965bafa05b4911c35b10ac
AlexanderGracetantiono <42028241+AlexanderGracetantiono@users.noreply.github.com>
2019-09-04 21:17:31 +0700

    Add files via upload

---
+++ Pengenalan PHP/tutorial1.php
@@ -0,0 +1,43 @@
+<?php
+//taruh code kalian disini
+// echo "Hello World";
+
+
+// $nama = "Alex";
+// echo "Hello ".$nama;
+
+
+// echo "<h1>Ini Header</h1>";
+
+//proses Variable dangan Value angka
+// $barang = 10;
+// $harga = 1200;
+// $jumlah = $harga*$barang;
+// echo "Total Harga Barang adalah : ".$jumlah."<br>";
+// echo "Total Harga Barang adalah : $jumlah <br>";
+// echo 'Total harga barang adalah : $jumlah';
+
+//proses Variable Huruf
+// $fname = "Joko";
+// $lname = " Susilo";
+// $full_Name = $fname.$lname;
+// echo $full_Name."<br>";
+// echo "$fname $lname";
+
+//cara pemrosesan alpha numerik
+// $angka =123;
+// $kalimat = "Hello ";
+// // echo "$kalimat $angka";
+// $test = "$kalimat $angka";
+// echo $test;
+
+// $angka1=3;
+// $angka2 = 5;
+
+// $jumlah = $angka1 + $angka2;
+// // echo $angka1+$angka2."<br>";
+// // echo $angka1-$angka2."<br>";
+// // echo $angka1/$angka2."<br>";
+// // echo $angka1*$angka2."<br>";
+// echo $jumlah;
+?>
